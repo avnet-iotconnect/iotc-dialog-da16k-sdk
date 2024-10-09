76b799cb0292411321154df526136deccce3cc6b
nik-markovic <nikola.m@gmail.com>
2024-10-09 12:50:34 -0500

    Logging cleanup

--- apps/common/examples/Network/IoTConnect_Client/src/iotc_app/iotc_generic_sdk_da16k_mosquitto_client.c
+++ apps/common/examples/Network/IoTConnect_Client/src/iotc_app/iotc_generic_sdk_da16k_mosquitto_client.c
@@ -43,18 +43,20 @@ static void iotc_device_deinit(void) {
 
 static void __mqtt_on_message(const char *buf, int len, const char *topic)
 {
-    IOTC_INFO("[%s] Msg Recv: topic=%s, msg=%s, len = %d\n\n" CLEAR_COLOR, __func__, topic, buf, len);
+	// Let the SDK decide whether to log it with the verbose flag
+    // IOTC_INFO("MQTT Received on topic: %s, msg: %.*s", topic, len, buf);
+	(void) topic;
 
     if (c2d_msg_cb) {
         c2d_msg_cb( (const unsigned char*) buf, (size_t) len);
     } else {
-        IOTC_INFO("%s: c2d_msg_cb == NULL\n" CLEAR_COLOR, __func__);
+        IOTC_ERROR("%s: c2d_msg_cb == NULL", __func__);
     }
 }
 
 static void __mqtt_on_disconnected(void)
 {
-    IOTC_INFO("[%s] MQTT disconnection callback!" CLEAR_COLOR, __func__);
+    IOTC_INFO("MQTT disconnection callback!");
 
     if (status_cb) {
         status_cb(IOTC_CS_MQTT_DISCONNECTED);
@@ -80,8 +82,11 @@ bool iotc_device_client_is_connected(void) {
 }
 
 int iotc_device_client_send_message_qos(const char* topic, const char *message, int qos) {
-    IOTC_WARN("%s: NOT IMPLEMENTED, defaulting to non-QOS\n\n", __func__);
-    // maybe we can implement by setting in NVRAM?
+
+	// Nik: This message seems to be redundant, but we should still be implementing a callback when published.
+    // IOTC_WARN("%s: NOT IMPLEMENTED, defaulting to non-QOS", __func__);
+
+	// maybe we can implement by setting in NVRAM?
     // but really should be kept with the message in the message Q
     (void) qos;
     return mqtt_sample_client_send(topic, message, true);
@@ -93,23 +98,21 @@ int iotc_device_client_send_message(const char *topic, const char *message) {
 
 static void __mqtt_on_publish(int mid)
 {
-    IOTC_INFO("[%s] message id %d has been published!\n\n" CLEAR_COLOR, __func__, mid);
+    IOTC_INFO("MQTT message id %d published!", mid);
 }
 
 static void __mqtt_on_connected(void)
 {
-    IOTC_INFO("[%s] MQTT connection callback!\n\n" CLEAR_COLOR, __func__);
+    IOTC_INFO("MQTT is Connected");
 
     is_connected = true;
 }
 
 static void __mqtt_on_subscribe(void)
 {
-    IOTC_INFO("[%s] topic subscribed!\n\n" CLEAR_COLOR, __func__);
-
     // signal IOTC_CS_MQTT_CONNECTED when broker is connected *and* the subscription succeeded
     if (is_connected == false) {
-        IOTC_ERROR("[%s] topic subscribed but not connected?\n\n" CLEAR_COLOR, __func__);
+        IOTC_ERROR("[%s] topic subscribed but not connected?" CLEAR_COLOR, __func__);
     }
 
     if (status_cb) {
@@ -121,7 +124,7 @@ static void __mqtt_on_subscribe(void)
 
 static void __mqtt_on_unsubscribe(void)
 {
-    IOTC_ERROR("\n\n[%s] topic unsubscribed unexpectedly!\n\n" CLEAR_COLOR, __func__);
+    IOTC_ERROR("[%s] topic unsubscribed unexpectedly!" CLEAR_COLOR, __func__);
 }
 
 // setup the MQTT client configuration
@@ -149,17 +152,17 @@ int iotc_device_client_connect(IotConnectDeviceClientConfig *c) {
     // Set the callbacks early
     c2d_msg_cb = c->c2d_msg_cb;
     if(c2d_msg_cb == NULL) {
-        IOTC_INFO("%s: c2d_msg_cb == NULL\n" CLEAR_COLOR, __func__);
+        IOTC_INFO("%s: c2d_msg_cb == NULL" CLEAR_COLOR, __func__);
     }
 
     status_cb = c->status_cb;
     if(status_cb == NULL) {
-        IOTC_INFO("%s: status_cb == NULL\n" CLEAR_COLOR, __func__);
+        IOTC_INFO("%s: status_cb == NULL" CLEAR_COLOR, __func__);
     }
 
     ret = mqtt_sample_client_init(__mqtt_on_message, __mqtt_on_publish, __mqtt_on_connected, __mqtt_on_disconnected, __mqtt_on_subscribe, __mqtt_on_unsubscribe);
     if (ret != 0) {
-        IOTC_ERROR("[%s] mqtt_sample_client_init() failed\n", __func__);
+        IOTC_ERROR("[%s] mqtt_sample_client_init() failed", __func__);
         goto cleanup;
     }
 
